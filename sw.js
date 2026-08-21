a323cbe4922e5c7127140a480a4512efd75d37dd
nightbro4-the-one-the-only <nightbro4-the-one-the-only@users.noreply.github.com>
2026-08-21 11:01:05 +0000

    deploy: 170d3da209a3625a2435b2bd44e8edaeebc33ead

--- sw.js
+++ sw.js
@@ -29,8 +29,19 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
       .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll())
+      .then((clients) => {
+        self.clients.claim();
+        // Notify all open tabs that a new version is ready
+        clients.forEach((client) => client.postMessage({ type: 'SW_UPDATED', version: CACHE }));
+      })
   );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener('fetch', (event) => {
